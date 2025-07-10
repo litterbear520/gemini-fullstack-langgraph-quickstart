5c7427938af7706d856bfaa3834446cfbd6d5986
littlebear <970219247@qq.com>
2025-07-10 13:28:34 +0800

    更新 picture.py 文件，修改二维码保存路径为 'qr3.png'，并更新生成的二维码内容为新的博客链接；更新 test-agent.ipynb 文件，简化代码并添加新的查询功能，输出西班牙在欧洲杯的历史战绩和排名信息。

--- picture.py
+++ picture.py
@@ -1,4 +1,4 @@
 from etool import ManagerQrcode
 
-qr_path = 'qr.png' # 保存路径
-ManagerQrcode.generate_english_qrcode(words="http://litterbear.fun/app/", save_path=qr_path) # 生成
+qr_path = 'qr3.png' # 保存路径
+ManagerQrcode.generate_english_qrcode(words="https://litterbear520.github.io/Blog/bloglist", save_path=qr_path) # 生成
